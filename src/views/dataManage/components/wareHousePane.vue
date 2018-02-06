6390298dab7b6e428fd9ec68adaf28807adecb4f
Yuanbo Liu <yuanbo@Yuanbos-MacBook-Air.local>
2018-02-06 20:49:04 +0800

    init from 3.62

---
+++ src/views/dataManage/components/wareHousePane.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="block">
+    <el-cascader placeholder="试试搜索：ods" :options="tables" filterable></el-cascader>
+    <el-table :data="table.values" style="width: 100%">
+        <el-table-column v-for="(item, key, index) in table.column" :label="item" :key="index"></el-table-column>
+    </el-table>
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+      return {
+        tables: null,
+        table: {
+            column: null,
+            values: null
+        },
+      }
+  },
+  created() {
+      this.getTableList()
+  },
+  methods: {
+    getTableList() {
+        // 获取到所有的列表
+    },
+    getColumnList() {
+
+    }
+  }
+}
+</script>
---
+++ src/views/dataManage/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="tab-container">
+    <el-tabs style='margin-top:15px;' v-model="activeName" type="border-card">
+      <el-tab-pane label="数据仓库" name="warehouse">
+        <keep-alive>
+          <ware-house-pane></ware-house-pane>
+        </keep-alive>
+      </el-tab-pane>
+      <el-tab-pane label="hdfs 目录" name="hdfs">
+        <keep-alive>
+          <tab-pane></tab-pane>
+        </keep-alive>
+      </el-tab-pane>
+      <el-tab-pane label="回收站" name="trash">
+        <keep-alive>
+          <tab-pane></tab-pane>
+        </keep-alive>
+      </el-tab-pane>
+    </el-tabs>
+  </div>
+</template>
+
+<script>
+import tabPane from './components/tabPane'
+import wareHousePane from './components/wareHousePane'
+
+export default {
+  name: 'tab',
+  components: { tabPane, wareHousePane},
+  data() {
+    return {
+      activeName: 'wareHouse',
+      createdTimes: 0
+    }
+  },
+  methods: {
+    showCreatedTimes() {
+      this.createdTimes = this.createdTimes + 1
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .tab-container{
+    margin: 30px;
+  }
+</style>
